ca00b00fdeaffdccee6764a52c0c015092075d5c
tglima <thg.limadesousa@gmail.com>
2023-04-17 20:36:45 -0300

    Adiciona o novos métodos e muda o export da class

--- src/app/utils/config.util.js
+++ src/app/utils/config.util.js
@@ -4,9 +4,27 @@ import constant from './constant.util';
 const envMode = process.env.NODE_ENV || constant.development;
 const path = `./config/env/${envMode}.env`;
 
+function getJsonFromTagConfig(tagConfig) {
+  try {
+    const configs = global.projectConfigs;
+    const row = configs.filter((data) => data.tag_config === tagConfig);
+    return row[0].json_config;
+  } catch (error) {
+    throw new Error(`Erro ao tentar ler as configs da tag: ${tagConfig}`);
+  }
+}
+
 class ConfigUtil {
   constructor() {
     dotenv.config({ path });
+  }
+
+  static getInstance() {
+    if (!ConfigUtil.instance) {
+      ConfigUtil.instance = new ConfigUtil();
+    }
+
+    return ConfigUtil.instance;
   }
 
   getNuVersion() {
@@ -28,6 +46,24 @@ class ConfigUtil {
   getMsgServerStarted() {
     return `Servidor iniciado - Express rodando na porta: ${this.getServerPort()}`;
   }
+
+  getTokenConfig() {
+    return getJsonFromTagConfig('tokenConfig');
+  }
+
+  getSellConfig() {
+    return getJsonFromTagConfig('sellConfig');
+  }
+
+  getRegularConfig() {
+    return getJsonFromTagConfig('regularConfig');
+  }
+
+  getMessageContactConfig() {
+    return getJsonFromTagConfig('messageContactConfig');
+  }
 }
 
-export default new ConfigUtil();
+const configUtil = ConfigUtil.getInstance();
+
+export default configUtil;
