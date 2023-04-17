4e67de56f1d490a525557e5aecdb2c185d3784a9
tglima <thg.limadesousa@gmail.com>
2023-04-17 20:38:22 -0300

    Refatorado para utilizar o método main()

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 /* eslint-disable no-console */
 import app from './app';
 import configUtil from './app/utils/config.util';
+import dbUtil from './app/utils/db.util';
 
-console.log(`Env mode: ${configUtil.getNodeEnv()}!`);
+async function main() {
+  await dbUtil.loadConfigsFromDB();
+  console.log(`Env mode: ${configUtil.getNodeEnv()}!`);
 
-app.listen(
-  configUtil.getServerPort(),
-  console.warn(`App listening on ${configUtil.getServerPort()}`)
-);
+  app.listen(
+    configUtil.getServerPort(),
+    console.warn(`App listening on ${configUtil.getServerPort()}`)
+  );
 
-console.log(configUtil.getMsgServerStarted());
+  console.log(configUtil.getMsgServerStarted());
+}
+
+main();
