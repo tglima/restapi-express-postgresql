f35299175a0e48e6861cbe62da6502e047fc3456
tglima <thg.limadesousa@gmail.com>
2023-04-23 20:04:09 -0300

    Implementa a class LogRepository Responsavél por salvar os logs da aplicação no banco de dados

---
+++ src/app/repositories/LogRepository.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-console */
+const { default: dbUtil } = require('../utils/db.util');
+
+/**
+ *
+ * Salva no banco de dados informações importantes da requisição
+ *
+ * @param {object} req O objeto request recebido pela API
+ * @param {object} res O objeto response retornado
+ * @param {Number} idUser O idUser identificado na requisição
+ * @param {Date} dtStart A data e hora de início da requisição
+ * @returns {void}
+ */
+exports.SaveLogDB = async (req, res, idUser, dtStart) => {
+  try {
+    const db = await dbUtil.getConnection();
+
+    let sql;
+
+    sql = 'INSERT INTO api_logs';
+    sql +=
+      '(url, id_user_register, req_headers, req_body, req_method, res_status_code, res_json_body, dt_start)';
+    sql += 'VALUES ($1,$2,$3,$4,$5,$6,$7,$8);';
+    const values = [
+      req.originalUrl,
+      idUser,
+      req.headers,
+      req.body,
+      req.method,
+      res.statusCode,
+      res.jsonBody,
+      dtStart,
+    ];
+
+    await db.query(sql, values);
+  } catch (error) {
+    console.error(error);
+  }
+};
