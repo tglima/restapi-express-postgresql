f7d65fc86c44799ad5bfcfbcdcdc11a85c91c335
tglima <thg.limadesousa@gmail.com>
2023-05-01 06:42:21 -0300

    Implementa AuthController

---
+++ src/app/controllers/AuthController.js
@@ -0,0 +1,40 @@
+const { default: ReturnDTO } = require('../dtos/ReturnDTO');
+const userRepository = require('../repositories/UserRepository');
+const constant = require('../utils/constant.util');
+const { default: util } = require('../utils/util');
+
+async function checkUserAndPassDB(reqBody) {
+  const resultFind = await userRepository.findByUsernameAndPass(
+    reqBody.username,
+    reqBody.password
+  );
+
+  if (!resultFind.wasSuccess && resultFind.error) {
+    return new ReturnDTO(401, false, constant.MsgStatus401);
+  }
+
+  if (!resultFind.wasSuccess || !resultFind.jsonBody) {
+    return new ReturnDTO(500, false, constant.MsgStatus500);
+  }
+
+  return new ReturnDTO(200, true, util.generateToken(resultFind.jsonBody));
+}
+
+class AuthController {
+  /**
+   *
+   * Responsável pela autenticação do usuário e disponibilizar o token
+   *
+   * @param {Object} req
+   * @param {Object} res
+   * @returns {ReturnDTO}
+   */
+  async auth(req, res) {
+    const dtStart = new Date().toJSON();
+    const response = await checkUserAndPassDB(req.body);
+    await util.saveLog(req, response, dtStart);
+    return res.status(response.statusCode).send(response.jsonBody);
+  }
+}
+
+module.exports = new AuthController();
