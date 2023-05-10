d61871423a5196f8a8abee0e002bb92dcc076502
tglima <thg.limadesousa@gmail.com>
2023-05-10 12:05:25 -0300

    Implementa o  ContactMessageController

---
+++ src/app/controllers/ContactMessageController.js
@@ -0,0 +1,43 @@
+import ReturnDTO from '../dtos/ReturnDTO';
+import util from '../utils/util';
+
+const contactMessageValidator = require('../validators/contactMessage.validator');
+const contactMessageRepository = require('../repositories/ContactMessageRepository');
+
+async function validateAndSaveMessage(req) {
+  let returnValidate = new ReturnDTO(0, true, []);
+  const message = req.body;
+
+  returnValidate = await contactMessageValidator.validateSaveMessage(message);
+
+  if (!returnValidate.wasSuccess) {
+    return new ReturnDTO(
+      400,
+      false,
+      JSON.parse(JSON.stringify(returnValidate.messages))
+    );
+  }
+
+  const resultSave = await contactMessageRepository.saveNew(message);
+
+  return resultSave;
+}
+
+class ContactMessageController {
+  /**
+   *
+   * Responsável por validar e salvar as mensagens de contato enviadas por qualquer usuário
+   *
+   * @param {Object} req
+   * @param {Object} res
+   * @returns {ReturnDTO}
+   */
+  async save(req, res) {
+    const dtStart = new Date().toJSON();
+    const response = await validateAndSaveMessage(req);
+    await util.saveLog(req, response, dtStart);
+    return res.status(response.statusCode).send(response.jsonBody);
+  }
+}
+
+export default new ContactMessageController();
