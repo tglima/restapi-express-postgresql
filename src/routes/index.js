c816d8bda4025486c483602c9da9d6622e030d72
tglima <thg.limadesousa@gmail.com>
2023-05-28 19:18:24 -0300

    Adiciona as novas rotas ao centralizador de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import AuthRoutes from './auth.routes';
 import HealthRoutes from './health.routes';
+import ProductRoutes from './product.routes';
 
 const routes = Router();
-routes.use('/health', HealthRoutes);
 routes.use('', AuthRoutes);
+routes.use('/health', HealthRoutes);
+routes.use('/product', ProductRoutes);
+
 export default routes;
