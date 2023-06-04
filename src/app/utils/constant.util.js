9256e33682fef835ca9af7f383b960d5f92def78
tglima <thg.limadesousa@gmail.com>
2023-06-04 11:58:47 -0300

    Refatora o arquivo para utilizar o padrão de class

--- src/app/utils/constant.util.js
+++ src/app/utils/constant.util.js
@@ -1,59 +1,64 @@
-export const development = 'development';
+let instance;
 
-export const MsgErrorInstance =
-  'Error: Não pode ser criada uma nova instância!!';
+class ConstantUtil {
+  constructor() {
+    this.development = 'development';
+    this.MsgErrorInstance = 'Error: Não pode ser criada uma nova instância!!';
+    this.MsgStatus201 = 'Registrado com sucesso!';
+    this.MsgStatus400 =
+      'Ocorreu uma falha/erro na sua requisição. Reveja os dados enviados e tente novamente!';
+    this.MsgStatus401 = 'Credenciais inválidas!';
+    this.MsgStatus401Alt = 'Usuário sem permissão ou Token inválido!';
+    this.MsgStatus404 = 'Item não encontrado!';
+    this.MsgStatus429 =
+      'Limite de requisições ultrapassado, por favor, aguarde.';
+    this.MsgStatus500 = 'Erro interno no servidor!';
+    this.MsgInvalidName = 'Nome inválido!';
+    this.MsgInvalidDate = 'A data informada é inválida!';
+    this.MsgInvalidDtBirth = 'A data de nascimento informada é inválida!';
+    this.MsgInvalidDtBirthAlt =
+      'A data de nascimento informada não corresponde a uma idade válida!';
+    this.MsgInvalidGender = 'Sexo inválido!';
+    this.MsgInvalidEmail = 'O e-mail informado é inválido!';
+    this.MsgInvalidQtCharMessage = `Obrigatório informar uma mensagem com pelo menos $1 caracteres!`;
+    this.MsgInvalidPhone = 'O número de telefone informado é inválido!';
+    this.MsgInvalidDDD = 'O número de DDD informado é inválido!';
+    this.MsgInvalidDoc = 'O documento informado é inválido!';
+    this.MsgNuDocumentUninformed = 'nuDocument Não informado!';
+    this.MsgIdCustomerUninformed = 'idCustomer Não informado!';
+    this.MsgRequiredId = 'Obrigatório informar o id';
+    this.MsgRequiredPhoneHasDDD =
+      'Ao informar o DDD é obrigatório informar o número do telefone!';
+    this.MsgRequiredDDDHasPhone =
+      'Ao informar o número do telefone é obrigatório informar o DDD!';
+    this.MsgRequiredPhoneOrEmail =
+      'Obrigatório informar o E-mail, ou DDD e Telefone';
+    this.defRegularConfig = {
+      idSuperUser: 0,
+      idSuperRole: 0,
+      idGuestUser: 0,
+      idGuestRole: 0,
+    };
+    this.defTokenConfig = {
+      tokenMinutesExpiration: 0,
+      tokenSecret: '',
+      tokenType: '',
+    };
+    this.defSellConfig = {
+      minAgeSellProduct: 0,
+      maxAgeSellProduct: 1,
+      nuYearsValProduct: 0,
+    };
+    this.defMessageContactConfig = { qtMinCharMessage: 0 };
+  }
 
-// Mensagens referente a Status
-export const MsgStatus201 = 'Registrado com sucesso!';
-export const MsgStatus400 =
-  'Ocorreu uma falha/erro na sua requisição. Reveja os dados enviados e tente novamente!';
-export const MsgStatus401 = 'Credenciais inválidas!';
-export const MsgStatus401Alt = 'Usuário sem permissão ou Token inválido!';
-export const MsgStatus404 = 'Item não encontrado!';
-export const MsgStatus429 =
-  'Limite de requisicoes ultrapassado, por favor, aguarde.';
-export const MsgStatus500 = 'Erro interno no servidor!';
+  static getInstance() {
+    if (!instance) {
+      instance = new ConstantUtil();
+      Object.freeze(instance);
+    }
+    return instance;
+  }
+}
 
-// Mensagens de itens inválidos
-export const MsgInvalidName = 'Nome inválido!';
-export const MsgInvalidDate = 'A data informada é inválida!';
-export const MsgInvalidDtBirth = 'A data de nascimento informada é inválida!';
-export const MsgInvalidDtBirthAlt =
-  'A data de nascimento informada não corresponde a uma idade válida!';
-export const MsgInvalidGender = 'Sexo inválido!';
-export const MsgInvalidEmail = 'O e-mail informado é inválidos!';
-export const MsgInvalidQtCharMessage = `Obrigatório informar uma mensagem com pelo menos $1 caracteres!`;
-export const MsgInvalidPhone = 'O número de telefone informado é inválido!';
-export const MsgInvalidDDD = 'O número de DDD informado é inválido!';
-export const MsgInvalidDoc = 'O documento informado é inválido!';
-
-// Mensagens de itens não informados
-export const MsgNuDocumentUninformed = 'nuDocument Não informado!';
-export const MsgIdCustomerUninformed = 'idCustomer Não informado!';
-
-// Mensagens de itens obrigatórios
-export const MsgRequiredId = 'Obrigatório informar o id';
-export const MsgRequiredPhoneHasDDD =
-  'Ao informar o DDD é obrigatório informar o número do telefone!';
-export const MsgRequiredDDDHasPhone =
-  'Ao informar o número do telefone é obrigatório informar o DDD!';
-export const MsgRequiredPhoneOrEmail =
-  'Obrigatório informar o E-mail, ou DDD e Telefone';
-
-export const defRegularConfig = {
-  idSuperUser: 0,
-  idSuperRole: 0,
-  idGuestUser: 0,
-  idGuestRole: 0,
-};
-export const defTokenConfig = {
-  tokenMinutesExpiration: 0,
-  tokenSecret: '',
-  tokenType: '',
-};
-export const defSellConfig = {
-  minAgeSellProduct: 0,
-  maxAgeSellProduct: 1,
-  nuYearsValProduct: 0,
-};
-export const defMessageContactConfig = { qtMinCharMessage: 0 };
+export default ConstantUtil.getInstance();
