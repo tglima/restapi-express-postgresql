77cdfbb4dbfd32d4861686e60d4d8efae2c4924b
tglima <thg.limadesousa@gmail.com>
2023-06-04 10:33:26 -0300

    Adiciona testes unitários para Product

--- __tests__/integration/moduleFolder/integration.test.js
+++ __tests__/integration/moduleFolder/integration.test.js
@@ -2,7 +2,36 @@ import app from '../../../src/app';
 import configUtil from '../../../src/app/utils/config.util';
 import dbUtil from '../../../src/app/utils/db.util';
 
+let token;
 const request = require('supertest');
+
+const getDatabaseCredentials = async () => {
+  const db = await dbUtil.getConnection();
+  let sql = 'SELECT ';
+  sql += 'NM_USER as username, DE_PASSWORD as password ';
+  sql += 'FROM USERS ';
+  sql += 'WHERE ';
+  sql += 'ID_ROLE = $1 AND IS_ACTIVE = true;';
+  const values = [2];
+  const resDB = await db.query(sql, values);
+  const user = resDB.rows[0];
+  db.release();
+  return user;
+};
+
+const getToken = async () => {
+  if (!token) {
+    const { username, password } = await getDatabaseCredentials();
+    const response = await request(app)
+      .post(`${configUtil.getUrlBaseApi()}/auth`)
+      .send({ username, password });
+    if (response.body && response.body.access_token) {
+      token = response.body.access_token;
+    }
+  }
+
+  return token;
+};
 
 describe('Running tests for /health', () => {
   it('Test get /health', async () => {
@@ -24,21 +53,44 @@ describe('Running tests for /auth', () => {
     expect(response.status).toEqual(401);
   });
   it('Test post /auth', async () => {
-    const db = await dbUtil.getConnection();
-    let sql = 'SELECT NM_USER, DE_PASSWORD ';
-    sql += 'FROM USERS ';
-    sql += 'WHERE ';
-    sql += 'ID_ROLE = $1 AND IS_ACTIVE = true;';
-    const values = [2];
-    const resDB = await db.query(sql, values);
-    const user = resDB.rows[0];
-    db.release();
+    const { username, password } = await getDatabaseCredentials();
     const response = await request(app)
       .post(`${configUtil.getUrlBaseApi()}/auth`)
-      .send({
-        username: user.nm_user,
-        password: user.de_password,
-      });
+      .send({ username, password });
     expect(response.status).toEqual(200);
+  });
+});
+
+describe('Running tests for /products', () => {
+  it('Test get product/find', async () => {
+    token = await getToken();
+    const response = await request(app)
+      .get(`${configUtil.getUrlBaseApi()}/product/find`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(response.status).toEqual(200);
+  });
+
+  it('Test get product/find/id=1', async () => {
+    token = await getToken();
+    const response = await request(app)
+      .get(`${configUtil.getUrlBaseApi()}/product/find/id=${1}`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(response.status).toEqual(200);
+  });
+
+  it('Test get product/find/id=', async () => {
+    token = await getToken();
+    const response = await request(app)
+      .get(`${configUtil.getUrlBaseApi()}/product/find/id=`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(response.status).toEqual(400);
+  });
+
+  it('Test get product/find/id=0', async () => {
+    token = await getToken();
+    const response = await request(app)
+      .get(`${configUtil.getUrlBaseApi()}/product/find/id=0`)
+      .set('Authorization', `Bearer ${token}`);
+    expect(response.status).toEqual(404);
   });
 });
--- __tests__/unit/unit.test.js
+++ __tests__/unit/unit.test.js
@@ -2,6 +2,7 @@ import ReturnDTO from '../../src/app/dtos/ReturnDTO';
 import { saveNew } from '../../src/app/repositories/ContactMessageRepository';
 import { checkPermissionEndpoint } from '../../src/app/repositories/ControlAccessRepository';
 import { saveLogDB } from '../../src/app/repositories/LogRepository';
+import ProductRepository from '../../src/app/repositories/ProductRepository';
 import { findByUsernameAndPass } from '../../src/app/repositories/UserRepository';
 import configUtil from '../../src/app/utils/config.util';
 import dbUtil from '../../src/app/utils/db.util';
@@ -62,6 +63,16 @@ describe('Unit testing for repositories', () => {
 
     const result = await saveNew(contactMessage);
     expect(result.wasSuccess).toEqual(true);
+  });
+
+  it('Test ProductRepository.findAllProducts', async () => {
+    const result = await ProductRepository.findAllProducts();
+    expect(result.wasSuccess).toEqual(true);
+  });
+
+  it('Test ProductRepository.findProductById', async () => {
+    const result = await ProductRepository.findProductById(0);
+    expect(result.wasSuccess).toEqual(false);
   });
 });
 
