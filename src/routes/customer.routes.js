e2c98c5d4ad0fbe95e09f872c35c49c556e21cda
tglima <thg.limadesousa@gmail.com>
2023-07-30 16:33:27 -0300

    Adiciona novas rotas Adicionou a rota /customer/save para salvar um novo registro Adicionou a rota /customer/update/id={id} para atualizar registros

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -4,4 +4,6 @@ import controller from '../app/controllers/CustomerController';
 const router = Router();
 router.get('/find/id=:id?', controller.findById);
 router.get('/find/document=:document?', controller.findByNuDocument);
+router.post('/save', controller.save);
+router.put('/update/id=:id', controller.update);
 export default router;
